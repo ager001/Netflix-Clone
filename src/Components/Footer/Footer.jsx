1e708623e7f0e974e2baf126dd1fa33b6b30d14f
Michael Ager Midenga <agermidenga@gmail.com>
2025-05-18 14:00:53 +0300

    day 3 of my netlix clone

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={facebook_icon} alt="" />
+      </div>
+    <ul>
+      <li>Audio Description</li>
+      <li>Help Center</li>
+      <li>Gift Cards</li>
+      <li>Contact Us</li>
+      <li>Help Center</li>
+      <li>Jobs</li>
+      <li>Terms of Use</li>
+      <li>Legal Notices</li>
+      <li>Cookie Preferences</li>
+      <li>Corporate Information</li>
+    </ul>
+    <p className='copyright-text'>&copy; 1997-2023 Netflix, Inc.</p>
     </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/Play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../Components/TitleCards/TitleCards'
+import Footer from '../../Components/Footer/Footer'
 
 
 const Home = () => {
@@ -29,6 +30,14 @@ const Home = () => {
           
         </div>
       </div>
+      <div className="more-cards">
+      <TitleCards title={"Blockbuster Movies"}/>
+      <TitleCards title={"Only on Netflix"}/>
+      <TitleCards title={"Upcoming"}/>
+      <TitleCards title={"Topic Picks for you"}/>
+      </div>
+
+      <Footer/>
       
     </div>
   )
